ec4e16ef91652a860b52c28f53c6f42ad054380a
Sandro Ciervo <sandro.ciervo@live.com>
2017-11-03 10:48:19 +0100

    Added projects

---
+++ ClassLibrary462/SimpleArithmetic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassLibrary462
+{
+    public class SimpleArithmetic
+    {
+        public decimal Addition(int x, int y)
+        {
+            return x + y;
+        }
+
+        public decimal Subtraction(int x, int y)
+        {
+            return x - y;
+        }
+
+        public decimal Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        public decimal Division(int x, int y, bool stupid = false)
+        {
+            if (stupid && y == 0)
+                return 0;
+
+            return x / y;
+        }
+    }
+}
---
+++ UnitTestProject462/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject462")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject462")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a786b8d7-a29a-4e58-b71e-60ada82292aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
